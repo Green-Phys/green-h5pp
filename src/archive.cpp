9ab85bce5a4d213e4dea8e1baadf7ffa95ccbfdb
Sergei Iskakov <sir.iskakoff@gmail.com>
2024-03-26 15:41:48 -0400

    put file name in quotes in error messages

--- src/archive.cpp
+++ src/archive.cpp
@@ -18,20 +18,20 @@ void green::h5pp::archive::open(const std::string& filename, const std::string& 
     throw hdf5_file_access_error("File is already opened. Please close current file before opening another.");
   }
   if (access_type != "r" && access_type != "w" && access_type != "a") {
-    throw hdf5_unknown_access_type_error("Unknown access type " + access_type + ". Should be 'r', 'w' or 'a'");
+    throw hdf5_unknown_access_type_error("Unknown access type '" + access_type + "'. Should be 'r', 'w' or 'a'");
   }
   bool file_exists = std::filesystem::exists(filename);
 
   if (access_type == "r") {
     if (!file_exists) {
-      throw hdf5_file_access_error("File " + filename + " does not exist.");
+      throw hdf5_file_access_error("File '" + filename + "' does not exist.");
     }
     htri_t info = H5Fis_hdf5(filename.c_str());
     if (info < 0) {
-      throw hdf5_file_access_error("Error accessing hdf5 file " + filename + ".");
+      throw hdf5_file_access_error("Error accessing hdf5 file '" + filename + "'.");
     }
     if (info == 0) {
-      throw not_hdf5_file_error(filename + " is not an HDF5 file.");
+      throw not_hdf5_file_error("'" + filename + "' is not an HDF5 file.");
     }
   }
   hid_t file = H5I_INVALID_HID;
@@ -43,7 +43,7 @@ void green::h5pp::archive::open(const std::string& filename, const std::string& 
   else if (access_type == "w")
     file = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
   if (file == H5I_INVALID_HID) {
-    throw hdf5_file_access_error("Can not open hdf5 file " + filename);
+    throw hdf5_file_access_error("Can not open hdf5 file '" + filename + "'");
   }
   file_id()    = file;
   current_id() = file;
@@ -56,7 +56,7 @@ green::h5pp::archive::~archive() {
 
 bool green::h5pp::archive::close() {
   if (H5Fclose(file_id()) < 0) {
-    throw hdf5_file_access_error("Can not close file " + _filename);
+    throw hdf5_file_access_error("Can not close file '" + _filename + "'");
   }
   file_id()    = H5I_INVALID_HID;
   current_id() = H5I_INVALID_HID;
